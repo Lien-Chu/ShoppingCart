66404577f5c45076d4fe21178031c3a1f8207db5
LIEN-CHU HUANG <vickykilumanga@gmail.com>
2023-05-12 15:39:08 +0200

    Added Customer Area

--- ShoppingCart/Program.cs
+++ ShoppingCart/Program.cs
@@ -37,7 +37,8 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
+
 app.MapRazorPages();
 
 app.Run();
